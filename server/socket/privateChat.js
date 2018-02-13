b4052c273c500284c72ad981245f2f1ff9d29ccc
Spectrum <taiwo.sunday@outmail.com>
2018-02-13 17:22:56 +0100

    added

--- server/socket/privateChat.js
+++ server/socket/privateChat.js
@@ -16,7 +16,7 @@ module.exports = function (io) {
       });
 
       io.emit('message display', {});
-      callback();
+      //callback();
       console.log(message);
     });
     socket.on('refresh', function(){
